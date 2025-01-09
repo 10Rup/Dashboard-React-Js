cd89b15fb8320f71b7a99f680487f147357de12c
10Rup <rupmandal.lspl@gmail.com>
2025-01-09 17:56:51 +0530

    day4
    day 4

--- react-tutorial/src/App.jsx
+++ react-tutorial/src/App.jsx
@@ -1,10 +1,27 @@
+import {useState} from 'react'
 import { UserProfile } from "./components/UserProfile"
 import {ConditionalRendering1,ConditionalRendering2} from './components/ConditionalRendering'
 import { EventHandling } from "./components/EventHandling"
 import { UsingStates1, UsingStates2 } from "./components/UsingStates"
-
+import { UseStateArray } from "./components/UseStateArray"
 
 export default function App(){
+
+    const [usersData, setUserData] = useState([
+        {
+            id:1,
+            name:'Ram',
+            email:"ram@gamil.com"
+        },
+        {
+            id:2,
+            name:"rony",
+            email:'rony@gmail.com'
+        },
+    ])
+
+
+
     return(
         <div>
            <h1>React App</h1> 
@@ -35,6 +52,11 @@ export default function App(){
                 <h2>Using State</h2>
                 <UsingStates1/>
                 <UsingStates2/>
+            </div>
+
+            <div>
+                <h2>Using States with Array</h2>
+                {usersData.map((user)=> <UseStateArray key={user.id} user_data={user}/>)}
             </div>
         </div>
     )
--- react-tutorial/src/components/UseStateArray.jsx
+++ react-tutorial/src/components/UseStateArray.jsx
@@ -1,9 +1,24 @@
+import PropTypes from 'prop-types'
+export function UseStateArray(props){
 
-
-export function UseStateArray(){
+    const {id,email,name} = props.user_data
     return(
-        <div>
             
+        <div>
+            {/* <label htmlFor="username">Name : </label>
+            <input type="text" name="username"  /> */}
+            <h3>User - {id}</h3>
+            <b>User Name : </b><span>{name}</span><br />
+            <b>User Email : </b><span>{email}</span><br />
+            <br />
         </div>
-    )
+)
+}
+
+UseStateArray.propTypes = {
+    user_data:PropTypes.shape({
+        id:PropTypes.string.isRequired,
+        name:PropTypes.string.isRequired,
+        email:PropTypes.string
+    })
 }
